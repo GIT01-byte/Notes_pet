1c581a36ce89cf5df89ae601170d1fcb6dff85ba
Timur33 <gajsint411@gmail.com>
2026-02-23 22:57:34 +0500

    UPDATE (US: time decorator)

--- users-service/utils/time_decorator.py
+++ users-service/utils/time_decorator.py
@@ -1,25 +1,11 @@
-import os
-from pathlib import Path
 import time
 import functools
 from datetime import datetime
-import contextvars
 
 from core.settings import settings
 
-# Переменная для хранения уровня вложенности
-indent_var = contextvars.ContextVar("indent", default=0)
-
 # Включение/выключение декораторов через переменную окружения
 ENABLE_TIME_DECORATOR = settings.app.enable_time_reports
-
-# Настройка путей
-base_path = Path(__file__).parent.parent
-full_path_dir = f"{base_path}/reports"
-
-# Создание директории reports если не существует
-if ENABLE_TIME_DECORATOR:
-    Path(full_path_dir).mkdir(parents=True, exist_ok=True)
 
 
 def time_all_methods(decorator):
@@ -39,9 +25,6 @@ def async_timed_report():
         
         @functools.wraps(func)
         async def wrapper(*args, **kwargs):
-            indent = indent_var.get()
-            indent_var.set(indent + 1) # Увеличиваем отступ для вложенных функций
-            
             start_time = time.perf_counter()
             try:
                 return await func(*args, **kwargs)
@@ -49,13 +32,7 @@ def async_timed_report():
                 end_time = time.perf_counter()
                 duration = end_time - start_time
                 timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                report_line = (f"[{timestamp}] Расположение: {func.__code__.co_filename} Функция: {func.__name__} | "
-                               f"Время выполнения: {duration:.4f} сек.\n")
-                
-                Path(full_path_dir).mkdir(parents=True, exist_ok=True)
-                with open(os.path.join(full_path_dir, "async_timed_report.txt"), "a", encoding="utf-8") as f:
-                    f.write(report_line)
-                indent_var.set(indent) # Возвращаем уровень назад
+                print(f"[{timestamp}] {func.__name__} | {duration:.4f} сек.")
         return wrapper
     return decorator
 
@@ -66,24 +43,14 @@ def sync_timed_report():
         
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            indent = indent_var.get()
-            indent_var.set(indent + 1) # Увеличиваем отступ для вложенных функций
-            
             start_time = time.perf_counter()
             try:
-                result = func(*args, **kwargs)
-                return result
+                return func(*args, **kwargs)
             finally:
                 end_time = time.perf_counter()
                 duration = end_time - start_time
                 timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                report_line = (f"[{timestamp}] Расположение: {func.__code__.co_filename} Функция: {func.__name__} | "
-                               f"Время выполнения: {duration:.4f} сек.\n")
-                
-                Path(full_path_dir).mkdir(parents=True, exist_ok=True)
-                with open(os.path.join(full_path_dir, "sync_timed_report.txt"), "a", encoding="utf-8") as f:
-                    f.write(report_line)
-                indent_var.set(indent) # Возвращаем уровень назад
+                print(f"[{timestamp}] {func.__name__} | {duration:.4f} сек.")
         return wrapper
     return decorator
 
